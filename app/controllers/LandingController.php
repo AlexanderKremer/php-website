3896bdec51890654b4b8e820b8fc989735e76a4a
Alexander Kremer <alexanderkremer47@gmail.com>
2016-06-15 11:06:31 +1200

    Initial commit

---
+++ app/controllers/LandingController.php
@@ -0,0 +1,77 @@
+<?php
+
+class LandingController {
+
+	// Properties (attributes)
+	private $emailMessage;
+	private $passwordMessage;
+
+	// Constructor
+	public function __construct() {
+
+		// If the user has submitted the registration form
+		if ( isset($_POST['new-account']) ) {
+			$this->validateRegistrationForm();
+		}
+
+	}
+
+
+	// Methods (functions)
+	public function registerAccount() {
+
+		// Validate the user's data
+
+		// Check the database to see if 
+		// E-Mail is in use
+
+		// Check the strength of the password
+
+		// Register the account OR show error messages
+
+		// If successful, log user in and redirect
+		// to their brand new stream page (account)
+
+
+	}
+
+	public function buildHTML() {
+
+		// Instantiate (create instance of) Plates library
+		$plates = new League\Plates\Engine('app/templates');
+
+		// Prepare a container for data
+		$data = [];
+
+		// If there is an E-Mail error
+		if ($this->emailMessage != '') {
+			$data['emailMessage'] = $this->emailMessage;
+		}
+
+		// If there is a message for password
+		if ($this->passwordMessage != '') {
+			$data['passwordMessage'] = $this->passwordMessage;
+		}
+
+		echo $plates->render('landing', $data);
+
+	}
+
+	private function validateRegistrationForm() {
+
+		// Make sure the E-mail has been provided and is valid
+		if ( $_POST['email'] == '' ) {
+			// E-Mail is invalid
+			$this->emailMessage = 'Invalid E-Mail';
+		}
+
+		// If the password is less than 8 characters long
+		if (strlen($_POST['password']) < 8 ) {
+			// Password is too short
+			$this->passwordMessage = 'Password must be at least 8 characters';
+
+		}
+
+	}
+
+}
---
+++ index.php
@@ -0,0 +1,60 @@
+<?php
+
+// Make everything in the vendor folder
+// available to use
+require 'vendor/autoload.php';
+
+
+
+// Has the user requested a page?
+// if( isset($_GET['page']) ) {
+// 	// Requested page
+// 	$page = $_GET['page'];
+// } else {
+// 	// Home page
+// 	$page = 'landing';
+// }
+
+	$page = isset($_GET['page']) ?$_GET['page'] : 'landing';
+
+// Load the appropriate files based on page
+switch($page) {
+
+	// Landing page
+	case 'landing':
+	case 'register':
+		require 'app/controllers/LandingController.php';
+		$controller = new LandingController();
+	break;
+
+	// About page
+	case 'about':
+		echo $plates->render('about');
+	break;
+
+	// Contact page
+	case 'contact':
+		echo $plates->render('contact');
+	break;
+
+	// Login page
+	case 'login':
+		echo $plates->render('login');
+	break;
+
+	// Stream page
+	case 'stream':
+		echo $plates->render('stream');
+	break;
+
+	default:
+		echo $plates->render('error404');
+	break;
+
+}
+
+
+$controller->buildHTML();
+
+
+
